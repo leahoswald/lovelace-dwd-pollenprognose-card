763375878793b32e8e5e92ad05cc779566a849ed
Leah Oswald <mail@leahoswald.de>
2022-02-13 12:22:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# pollenprognose-card
+A Lovelace custom card for [custom component DWD Pollenflug](https://github.com/mampfes/hacs_dwd_pollenflug) in Home Assistant based on the great work of @isabellaalstrom [pollenprognos-card](https://github.com/isabellaalstrom/lovelace-pollenprognos-card) and @TekniskSupport.
+
+<b>You need to manually move the folder `pollen_img` directly inside your `www` folder for the images to appear.</b>
+
+## Installation
+
+For installation instructions [see this guide](https://github.com/thomasloven/hass-config/wiki/Lovelace-Plugins).
+
+## Example usage
+Pick the allergens you want to display.
+
+For ui-mode:
+```yaml
+type: custom:pollenprognose-card
+region_desc: Oberrhein und unteres Neckartal
+region_id: 111
+allergens:
+  - Birke
+  - Erle
+  - Hasel
+  - Esche
+  - Graeser
+  - Roggen
+  - Beifuss
+  - Ambrosia
+```
+
+For yaml-mode:
+```yaml
+- type: custom:pollenprognose-card
+  region_desc: Oberrhein und unteres Neckartal
+  region_id: 111
+  allergens:
+    - Birke
+    - Erle
+    - Hasel
+    - Esche
+    - Graeser
+    - Roggen
+    - Beifuss
+    - Ambrosia
+```
+
+Usage in a view:
+```yaml
+title: My awesome Lovelace!
+resources:
+  - url: /local/pollenprognose-card.js
+    type: module
+views:
+  title: My view
+  cards:
+- type: custom:pollenprognose-card
+  region_desc: Oberrhein und unteres Neckartal
+  region_id: 111
+  allergens:
+    - Birke
+    - Erle
+    - Hasel
+    - Esche
+    - Graeser
+    - Roggen
+    - Beifuss
+    - Ambrosia
+```
+
+## Options
+
+| Name | Type | Default | Description
+| ---- | ---- | ------- | -----------
+| type | string | **Required** | `custom:home-assistant-pollen-card`
+| region_desc | string | **Required** | Name of the region, is only used for display in header.
+| region_id | string | **Required** | The region id as [defined by DWD](https://opendata.dwd.de/climate_environment/health/alerts/Beschreibung_pollen_s31fg.pdf) (same as in DWD Pollenflug component).
+| allergens | list | **Required** | List of allergens for which you have sensors
+| title | boolean | **Optional** | Set to `false` to remove the heading from the card
+| show_state | boolean | **Optional** | Set to `false` if you don't want to show the state text under the images.
+
+### Example of the card with all allergens presented
+![2021-04-23 14_24_23-Admin - Home Assistant](https://user-images.githubusercontent.com/22006797/115870566-b4e91080-a43f-11eb-843e-1f5efbcc2a84.png)
+
+
---
+++ pollenprognose-card.js
@@ -0,0 +1,172 @@
+const LitElement = customElements.get('home-assistant-main')
+  ? Object.getPrototypeOf(customElements.get('home-assistant-main'))
+  : Object.getPrototypeOf(customElements.get('hui-view'));
+const html = LitElement.prototype.html;
+
+class PollenPrognoseCard  extends LitElement {
+
+  setConfig(config) {
+    if (!config.allergens) {
+      throw new Error('Please define allergens (list)');
+    }
+    if (!config.region_desc) {
+      throw new Error('Please define region_desc');
+    }
+    if (!config.region_id) {
+      throw new Error('Please define region_id');
+    }
+    if (config.threshold && (typeof(config.threshold) != 'number')) {
+      throw new Error('Threshold must be a number')
+    }
+    this.config = config;
+  }
+
+  render(){
+    if(this.sensors.length < 1) {
+      console.log("No sensor data not rendering card.")
+      return;
+    }
+    return html
+    `
+    ${this._renderMinimalStyle()}
+    ${this._renderMinimalCard()}
+
+    `
+  }
+
+  _renderMinimalCard(){
+    return html
+    `
+    <ha-card>
+      ${this.config.title == null || this.config.title == true ?
+      html`
+      <div class="header">
+        <div class="name">
+        ${this.header}
+        </div>
+      </div>`
+      : ""
+    }
+      <div class="flex-container">
+        ${this.sensors.map(sensor => html`
+        <div class="sensor">
+          <p class="box">${sensor.allergen}</p>
+          <img class="box" src="/local/pollen_img/${sensor.allergen.toLowerCase()}_${sensor.forecast.state == "unknown" || sensor.forecast.state == "unavailable" ? 0 : sensor.forecast.state}.svg"/>
+          ${this.config.show_state == true || this.config.show_state == null
+            ? html`<p class="box">${sensor.forecast.state == "unknown" ? sensor.forecast.state : sensor.forecast.attributes.state_today_desc}</p>`
+            : ""}
+        </div>
+      `)}
+      </div>
+    </ha-card>
+    `
+  }
+
+  _renderMinimalStyle() {
+    return html
+    `
+    <style>
+    ha-card {
+      padding: 16px;
+    }
+    .header {
+      padding: 0;
+      @apply --paper-font-headline;
+      line-height: 40px;
+      color: var(--primary-text-color);
+      padding: 4px 0 12px;
+    }
+    .forecast {
+      width: 100%;
+      padding: 7px;
+      height: 100%;
+    }
+    td {
+      padding: 3px;
+      text-align: center;
+    }
+    .flex-container {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      text-align: center;
+      justify-content: space-evenly;
+      align-items: center;
+    }
+    .sensor {
+      margin: 10px;
+      flex: 1 1 0;
+      flex-direction: column;
+      justify-content: space-evenly;
+      display: flex;
+      align-self: flex-start;
+    }
+    @supports not (-ms-flex: 1) {
+      .flex-container {
+        height: auto; /* 2 */
+        // min-height: 24em; /* 2 */
+      }
+    }
+    img {
+      max-height: 50px;
+    }
+    .sensor {
+      display: block;
+      min-width: 16.66%;
+      flex: 1;
+    }
+    </style>`
+  }
+
+  
+  _tryParseInt(str,defaultValue) {
+    var retValue = defaultValue;
+    if(str !== null) {
+        if(str.length > 0) {
+            if (!isNaN(str)) {
+                retValue = parseInt(str);
+            }
+        }
+    }
+    return retValue;
+  }
+  set hass(hass) {
+    
+    this._hass = hass;
+    var sensors = [];
+
+    if (this.config.title == null || this.config.title == true) {
+      var header_region = this.config.region_desc
+      this.header = `Pollenprognose ${header_region.charAt(0).toUpperCase() + header_region.slice(1)}`;
+    }
+
+    const region_id = this.config.region_id;
+
+    const allergens = this.config.allergens;
+    for (var i = 0; i < allergens.length; i++) {
+      var dict = {};
+      dict.allergen = (allergens[i].charAt(0).toUpperCase() + allergens[i].slice(1));
+      var allergen = allergens[i].replace(' / ', '_').toLowerCase();
+
+      dict.forecast = hass.states[`sensor.pollenflug_${allergen}_${region_id}`]
+      if (dict.forecast.state == "unknown") {
+        if (dict.forecast === undefined) continue;
+        // var log_text = `A sensor for "${dict.allergen}" is returning unknown, you should probably check your config for that sensor in the custom component.`;
+        // console.log(log_text)
+      }
+
+      sensors.push(dict)
+    }
+
+    this.sensors = sensors;
+    this.requestUpdate();
+  }
+
+  // @TODO: This requires more intelligent logic
+  getCardSize() {
+    return 1;
+  }
+}
+
+class HAPC extends PollenPrognoseCard {} ;
+customElements.define('pollenprognose-card', PollenPrognoseCard);
